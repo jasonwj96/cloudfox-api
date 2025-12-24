ab247886e339fe43acdbca5c55e72f3cea46e4cb
Jason Wedderburn <jasonwj96@gmail.com>
2025-12-23 19:42:45 -0500

    Login query fix

--- internal/http/controllers/login_controller.go
+++ internal/http/controllers/login_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"cloudfox-api/internal/service/model"
 	"net/http"
 
 	"cloudfox-api/internal/repository"
@@ -17,14 +18,28 @@ func NewLoginController(accountRepository *repository.AccountRepository) *LoginC
 }
 
 func (c *LoginController) LoginUser(ctx *gin.Context) {
-	accountID := "85f13ac1-9417-4b05-a577-fc5737bf330d"
 
-	account, err := c.accountRepository.GetById(ctx.Request.Context(), accountID)
+	var request model.LoginRequest
+
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "invalid request body",
+		})
+
+		return
+	}
+
+	account, err := c.accountRepository.GetById(
+		ctx.Request.Context(),
+		request.AccountID,
+	)
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, gin.H{
 			"error": "internal server error",
 		})
+
 		return
 	}
 
@@ -32,12 +47,17 @@ func (c *LoginController) LoginUser(ctx *gin.Context) {
 		ctx.JSON(http.StatusUnauthorized, gin.H{
 			"error": "invalid credentials",
 		})
+
 		return
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"id":       account.ID,
-		"username": account.Username,
-		"email":    account.Email,
+		"id":           account.ID,
+		"username":     account.Username,
+		"fullname":     account.Fullname,
+		"email":        account.Email,
+		"creationDate": account.CreationDate,
+		"phoneNumber":  account.PhoneNumber,
+		"active":       account.Active,
 	})
 }
